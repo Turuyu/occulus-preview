19645f21e954bb0a2c6ea08997af3f82b9f1d3cf
Luis Guillermo Toraya Novelo <luistoraya@MacBook-Air-de-Luis.local>
2026-05-05 09:56:37 -0600

    ci: add github pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://turuyu.github.io',
+  base: '/occulus-preview',
   vite: {
     plugins: [tailwindcss()]
   }
